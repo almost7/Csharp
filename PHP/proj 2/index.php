60ca79535445ed79df4bc15c1756ed18b4823c5f
T0114404@ciber.local <T0114404@ciber.local>
2019-02-13 20:33:30 +0000

    projetos 2 e 3 de PHP

--- PHP/proj 2/index.php
+++ PHP/proj 2/index.php
@@ -1,38 +1,24 @@
-<?php
-for($i=1;$i<11;$i++){
-    echo "$i ";
-}
-echo"<br>";
-for($i=0;$i<21;$i++){
-    echo "<ul> <li>$i</li> </ul>";
-}
-echo"<br>";
-for($i=0;$i<101;$i+=2){
-    echo "$i ";
-}
-echo"<br>";
-echo"<br>";
-$b=6;
-for($i=1;$i<11;$i++){
-    echo "$b x $i = ".$i*$b;
+﻿<?php
+    /*for($i=1;$i<11;$i++){
+        echo "$i ";
+    }
     echo"<br>";
-}
-echo"<br>";
-for($n=1;$n<100;$n++){
-    $a=0;
-    for($i=1;$i<($n+1);$i++){
-        if($n%$i==0){
-            $a++;
-        }
+    for($i=0;$i<21;$i++){
+        echo "<ul> <li>$i</li> </ul>";
     }
-    if($a==2){    
-        echo "$n  ";
+    echo"<br>";
+    for($i=0;$i<101;$i+=2){
+        echo "$i ";
     }
-}
-echo"<br>";
-$cont = 0;
-while($cont<100){
-    for($n=1;$cont<100;$n++){
+    echo"<br>";
+    echo"<br>";
+    $b=6;
+    for($i=1;$i<11;$i++){
+        echo "$b x $i = ".$i*$b;
+        echo"<br>";
+    }
+    echo"<br>";
+    for($n=1;$n<100;$n++){
         $a=0;
         for($i=1;$i<($n+1);$i++){
             if($n%$i==0){
@@ -41,33 +27,73 @@ while($cont<100){
         }
         if($a==2){    
             echo "$n  ";
-            $cont++;
         }
     }
-}
-echo"<br>";
-echo"<br>";
-$palindromo = "pap";
-if (strrev($palindromo) == $palindromo){   
-    echo"a palavra $palindromo é um palindromo";
-}
-else{
-    echo"a palavra $palindromo não é um palindromo";
-}
-echo"<br>";
-echo"<br>";
-$capicua = "olo";
-if (strrev($capicua) == $capicua){   
-    if(is_integer($capicua)){
-        echo"o número $capicua é uma capicua";
+    echo"<br>";
+    $cont = 0;
+    while($cont<100){
+        for($n=1;$cont<100;$n++){
+            $a=0;
+            for($i=1;$i<($n+1);$i++){
+                if($n%$i==0){
+                    $a++;
+                }
+            }
+            if($a==2){    
+                echo "$n  ";
+                $cont++;
+            }
+        }
+    }*/
+    echo"<br>";
+    echo"<br>";
+    $pal = "ABC";
+    $inv = "";
+    for($i=0;$i<3;$i++)
+    {
+        $inv = $pal[$i].$inv;
+    }    
+    if (!strcmp($pal,$inv))
+    {
+        echo "é palindromo";
+    }
+    else
+    {    
+        echo "não é palindromo";
+    }
+    /*$palindromo = "pap";
+    if (strrev($palindromo) == $palindromo){   
+        echo"a palavra $palindromo é um palindromo";
+    }
+    else{
+        echo"a palavra $palindromo não é um palindromo";
+    }*/
+    echo"<br>";
+    echo"<br>";
+    $num = 121;
+    $inv = 0;
+    $original = $num;
+    while($num != 0){
+        $resto = $num % 10;
+        $num = (int)($num / 10);
+        $inv= $inv * 10 + $resto;
+    }
+    if ($original==$inv)
+        echo "é capicua";
+    else
+        echo "não é capicua";
+    /*$capicua = "olo";
+    if (strrev($capicua) == $capicua){   
+        if(is_integer($capicua)){
+            echo"o número $capicua é uma capicua";
+        }
+        else{
+            echo"\"$capicua\" não é capicua";
+        }
     }
     else{
         echo"\"$capicua\" não é capicua";
-    }
-}
-else{
-    echo"\"$capicua\" não é capicua";
-}
-echo"<br>";
-echo"<br>";
+    }*/
+    echo"<br>";
+    echo"<br>";
 ?>  
---
+++ PHP/proj3/index.php
@@ -0,0 +1,13 @@
+﻿<?php
+    function capicua(int $num) : bool{
+        $num = 121;
+        $inv = 0;
+        $original = $num;
+        while($num != 0){
+            $resto = $num % 10;
+            $num = (int)($num / 10);
+            $inv= $inv * 10 + $resto;
+        }
+        return $original!=$inv;
+    }
+?>
