48d6dad716a719b3f087afd4b4b22233b2c3bdbe
T0114404@ciber.local <T0114404@ciber.local>
2019-02-14 23:02:40 +0000

    primeiro windows form

---
+++ WindowsForms/WindowsFormsApp1/frmCumprimentos.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class frmCumprimentos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btnClique = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(26, 26);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(100, 20);
+            this.txt1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(26, 66);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // btnClique
+            // 
+            this.btnClique.Location = new System.Drawing.Point(146, 45);
+            this.btnClique.Name = "btnClique";
+            this.btnClique.Size = new System.Drawing.Size(75, 23);
+            this.btnClique.TabIndex = 2;
+            this.btnClique.Text = "Clique";
+            this.btnClique.UseVisualStyleBackColor = true;
+            this.btnClique.Click += new System.EventHandler(this.btnClique_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(82, 140);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpar.TabIndex = 4;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 109);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "label1";
+            // 
+            // frmCumprimentos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(236, 169);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnClique);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.txt1);
+            this.Name = "frmCumprimentos";
+            this.Text = "frmCumprimentos";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btnClique;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ WindowsForms/WindowsFormsApp1/frmCumprimentos.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class frmCumprimentos : Form
+    {
+        public frmCumprimentos()
+        {
+            InitializeComponent();
+            this.Height = 137;
+            this.Width = 252;
+            this.Text = "Cumprimentos";
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+        }
+
+        private void btnClique_Click(object sender, EventArgs e)
+        {
+            label1.Text = string.Format("{0} {1}", txt1.Text , textBox2.Text);
+            this.Height = 208;
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            txt1.Clear();
+            textBox2.Clear();
+            this.Height = 137;
+        }
+    }
+}
